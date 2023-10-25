8f8228cfba8d56c190cac8b00f33b9a5d20ce396
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-25 22:12:29 +0200

    modifiche e stampa di php

--- richiesta.php
+++ richiesta.php
@@ -1,2 +1,31 @@
 <?php
+
+$paragrafo = $_POST['paragrafo'];
+
+echo 'il tuo paragrafo è' .$paragrafo;
+
+$parolaDaCensurare = $_POST["parola"];
+
+echo 'la tua parola censurata è' .$parolaDaCensurare;
+
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Richiesta php</title>
+
+</head>
+
+<body>
+    
+</body>
+
+</html>
